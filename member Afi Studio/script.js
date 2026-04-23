8121d29c9d400728c87df8363efff57084d3f7b4
Ali Sastro123 <alisastro103@gmail.com>
2026-04-23 20:51:14 +0800

    Add files via upload

---
+++ member Afi Studio/script.js
@@ -0,0 +1,211 @@
+// DATA MEMBER AFI STUDIO
+const dataMember = {
+    "gen-1": [],
+    "gen-2": [
+        { 
+            nama: "kenzuu", 
+            foto: "profile/kenzuu.png", 
+            spesialis: "Artist, Animator, dan Render", 
+            identitas: "🦈",
+            generasi: "Generasi ke-2",
+            socials: {
+                yt: "https://youtube.com/@ikenjuk?si=DqqQaChmE1ZD7iZ5",
+                ig: "",
+                fb: "",
+                tk: "https://www.tiktok.com/@kenjuuk?_r=1&_t=ZS-95VGWPigg0P",
+                wa: "https://whatsapp.com/channel/0029VbB2PvsCBtxAJaIXRi3L",
+                dc: ""
+            }
+        },
+    
+      { 
+            nama: "Brentt", 
+            foto: "profile/breent.png", 
+            spesialis: "Animation, 3D Modeling, Music Producer, Editing, dan render maker", 
+            identitas: "🚀",
+            generasi: "Generasi ke-2",
+            socials: {
+                yt: "https://youtube.com/@brentnaoki",
+                ig: "https://www.instagram.com/brentnaoki",
+                fb: "",
+                tk: "https://www.tiktok.com/@brentnaoki",
+                wa: "https://whatsapp.com/channel/0029Vb7SnQLHAdNMcQSzvG0N",
+                dc: "https://discord.com/users/947990324251328564"
+            }
+        },
+    
+        { 
+            nama: "Excha", 
+            foto: "profile/excha.png", 
+            spesialis: "Animation, Art digital, dubbing, sing, Voice actor, dan panjat pohon ", 
+            identitas: "🍺",
+            generasi: "Generasi ke-2",
+            socials: {
+                yt: "https://www.youtube.com/@Exchaakatsy",
+                ig: "",
+                fb: "",
+                tk: "https://www.tiktok.com/@exchaardefa?_r=1&_t=ZS-95XB9jSJX0y",
+                wa: "https://whatsapp.com/channel/0029VbBDQXQKAwEsj0FPgE2R",
+                dc: "https://discord.com/users/947990324251328564"
+            }
+        }
+    
+    ],
+    "gen-3": [
+        { 
+            nama: "NUKE51", 
+            foto: "profile/nuke51.png", 
+            spesialis: "Animasi dan Rendering", 
+            identitas: "🚦",
+            generasi: "Generasi ke-3",
+            socials: {
+                yt: "https://youtube.com/@nukeganteng",
+                ig: "https://www.instagram.com/kawista_51",
+                fb: "https://facebook.com/nuke",
+                tk: "https://www.tiktok.com/@nukedyz",
+                wa: "https://wa.me/628895457589",
+                dc: "https://discordapp.com/users/780727070610554880"
+            }
+        },
+    
+        { 
+            nama: "Zipixe", 
+            foto: "profile/zin.png", 
+            spesialis: "Rendering, animasi, dan Tidur💤", 
+            identitas: "🪼",
+            generasi: "Generasi ke-3",
+            socials: {
+                yt: "https://youtube.com/@zipixe?si=VrOShMenvHfAYK87",
+                ig: "https://www.instagram.com/zipixe_?igsh=aWx5MjR2aTV6bGRo",
+                fb: "",
+                tk: "https://www.tiktok.com/@zipixe_",
+                wa: "https://whatsapp.com/channel/0029VbAZF6l8vd1OVho3nX1b",
+                dc: ""
+            }
+        },
+    
+        { 
+            nama: "Zena", 
+            foto: "profile/zena.png", 
+            spesialis: "Rendering dan animasi", 
+            identitas: "🌑",
+            generasi: "Generasi ke-3",
+            socials: {
+                yt: "https://youtube.com/@vihori_chin?si=nrebvHHJKxN7WXKh",
+                ig: "https://www.instagram.com/zena.aja._?igsh=MWp0ZzBqa3RzNHIzdQ==",
+                fb: "",
+                tk: "https://www.tiktok.com/@zena_aja1?_r=1&_t=ZS-95UhZ9dzpIt",
+                wa: "https://whatsapp.com/channel/0029VbCcPHHLCoX5BDcYlo3A",
+                dc: "https://discord.gg/PUhGCyzy"
+            }
+        }
+    ],
+    "orang-random": [
+    { 
+            nama: "RPC.Dina", 
+            foto: "profile/dina.png", 
+            spesialis: "Rendering", 
+            identitas: "🦖",
+            generasi: "-",
+            socials: {
+                yt: "https://youtube.com/@dina_ganteng?si=7SpHDaCnVBpLRlow",
+                ig: "https://www.instagram.com/dina_ganteng1?igsh=cW8yYzZnbjZlcm93",
+                fb: "https://www.facebook.com/share/1BKqhs2ENB/",
+                tk: "https://tiktok.com/@rpc.dina7",
+                wa: "https://whatsapp.com/channel/0029VbBP03J4CrfoKdu7CF2k",
+                dc: "https://discord.gg/pt3zKCDU"
+            }
+        }
+    ] // Tempat untuk menambahkan data orang random di masa depan
+};
+
+function getIcon(type) {
+    const icons = {
+        yt: { color: "#FF0000", svg: `<svg viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>` },
+        ig: { color: "#E1306C", svg: `<svg viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.981 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z"/></svg>` },
+        fb: { color: "#1877F2", svg: `<svg viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>` },
+        tk: { color: "#000000", svg: `<svg viewBox="0 0 24 24"><path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.9-.32-1.98-.23-2.81.36-.54.38-.89.96-.99 1.6-.13.58-.1 1.18.06 1.75.22.82.75 1.54 1.45 2.02.62.43 1.38.61 2.14.57 1.17-.05 2.24-.74 2.78-1.77.34-.64.48-1.36.48-2.08V.02z"/></svg>` },
+        wa: { color: "#25D366", svg: `<svg viewBox="0 0 24 24"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"/></svg>` },
+        dc: { color: "#5865F2", svg: `<svg viewBox="0 0 24 24"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037 19.736 19.736 0 0 0-4.885 1.515.069.069 0 0 0-.032.027C.533 9.048-.32 13.579.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128c.125-.094.25-.192.37-.291a.074.074 0 0 1 .077-.01c3.927 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.196.37.291a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 1-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"/></svg>` }
+    };
+    return icons[type] || null;
+}
+
+function renderMembers() {
+    for (let genId in dataMember) {
+        const container = document.getElementById(genId);
+        if (!container) continue;
+        container.innerHTML = ""; 
+        dataMember[genId].forEach((member, index) => {
+            container.innerHTML += `
+                <div class="id-card" onclick="openModal('${genId}', ${index})">
+                    <img src="${member.foto}" class="profile-img">
+                    <div class="member-name-box"><span class="member-name">${member.nama}</span></div>
+                </div>`;
+        });
+    }
+}
+
+function openModal(gen, index) {
+    const m = dataMember[gen][index];
+    const modal = document.getElementById('memberModal');
+    
+    let socialHtml = "";
+    for (let key in m.socials) {
+        const link = m.socials[key];
+        if (link && link.trim() !== "") {
+            const iconData = getIcon(key);
+            socialHtml += `<a href="${link}" target="_blank" class="social-btn" style="background-color: ${iconData.color}">${iconData.svg}</a>`;
+        }
+    }
+
+    document.getElementById('modalBody').innerHTML = `
+        <div class="modal-banner">
+            <img src="${m.foto}">
+        </div>
+        <div class="modal-details">
+            <div class="status-icon">${m.identitas}</div>
+            <h2 class="modal-name">${m.nama}</h2>
+            <div class="modal-divider"></div> 
+            <ul class="modal-subtext" style="list-style-type: disc; padding-left: 20px;">
+                <li>${m.spesialis}</li>
+                <li>${m.generasi}</li>
+            </ul>
+            <div class="social-container">
+                ${socialHtml}
+            </div>
+        </div>`;
+    
+    modal.style.display = "flex";
+    setTimeout(() => modal.classList.add('active'), 10);
+}
+
+function closeModal() {
+    const modal = document.getElementById('memberModal');
+    modal.classList.remove('active');
+    setTimeout(() => modal.style.display = "none", 400);
+}
+
+function toggleFolder(e) { e.classList.toggle('active'); }
+
+let chatTimeout;
+function showChatBubble() {
+    const bubble = document.getElementById('chatBubble');
+    const messages = [
+        "Haloo bangg👋🏻", 
+        "join sini bareng member Afi", 
+        "makasih ya udah kunjung website kami", 
+        "daftar untuk bergabung dengan member Afi lainnya"
+    ];
+    
+    const randomMsg = messages[Math.floor(Math.random() * messages.length)];
+    bubble.innerText = randomMsg;
+    bubble.classList.add('show');
+    
+    clearTimeout(chatTimeout);
+    chatTimeout = setTimeout(() => {
+        bubble.classList.remove('show');
+    }, 3000);
+}
+
+renderMembers();
